3ce71d3146ccc16f358e10913e7c78e6b4d63fcb
Danielochoacarmona9821 <161092883+Danielochoacarmona9821@users.noreply.github.com>
2024-03-11 20:27:05 -0500

    Add files via upload

---
+++ taller_de_desmpeño_01.py
@@ -0,0 +1,78 @@
+# Ejercicio 01
+
+a = (5 + 5) * 5
+b = 5 + (5 * 5)
+c = 5 % 2
+d = (5 - (6 / 3)) * 2
+
+print(f"{a}, {b}, {c}, {d}")
+
+
+# Ejercicio 02
+
+a = 7
+b = 3
+c = (a != b)
+d = (a==b)
+
+print(f"{c}, {d}, {a > b}, {(a+b) < 2}, {(a > 7 and b == 3)}, {a > 7 or b == 3}, {a < 7 or b != 3}")
+
+
+# Ejercicio 03
+  # Caso 01
+
+a = 3
+b = 2
+c = 1
+print(f"{a}, {b}, {c}")
+
+  # Caso 02
+
+a = 1
+b = 3
+c = 2
+print(f"{a}, {b}, {c}")
+
+  # Caso 03
+
+a = 1
+b = 2
+c = 3
+print(f"{a}, {b}, {c}")
+
+
+# Ejercicio 04
+
+nota_1 = float(input("ingres su nota del taller 1:"))
+nota_2 = float(input("ingresse su nota del taller 2:"))
+nota_3 = float(input("ingrese su nota del cuestionario 1:"))
+nota_4 = float(input("ingrese su nota del cuestionario 2:"))
+nota_5 = float(input("ingrese su nota del taller final:"))
+
+nota = float(nota_1 + nota_2 +nota_3 +nota_4 + nota_5) / 5
+
+print(f"su nota final es:", nota)
+
+
+# Ejercicio 05
+
+metro = int(input("ingrese cantidad de metros:"))
+convertir = metro * 1609
+print(f"es igual a {convertir}, millas")
+
+
+# Ejercicio 06
+
+
+
+
+
+# Ejercicio 07
+
+dolares = int(input("ingrese la cantida de dolares a convertir:"))
+convertir = dolares * 4000
+print(f"es igual a {convertir}  pesos colombianos")
+
+
+
+
